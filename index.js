cc0d0931598c01ffc8577d1f1d956241d68d9f58
Rishabh Mishra <mishrarishabh865@gmail.com>
2017-11-03 23:58:21 +0530

    Add route for rick and morty episodes

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express')
+const rickAndMorty = require('./data/rick_and_morty.json')
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
   res.send("Node api server for react-ssr App")
+})
+
+app.get('/rick-and-morty', (req, res)=>{
+  res.send(rickAndMorty)
 })
 
 app.listen(PORT, () => {
